95163c26ce5274861fc3afa8a8a53e1f713224b5
CesarTavares1 <cesartavaresfilho9@gmail.com>
2026-04-13 21:07:27 -0300

    feat(p1): obtem condicoes atuais a partir de coords

--- open-meteo.js
+++ open-meteo.js
@@ -5,14 +5,26 @@ const url2= "https://api.open-meteo.com/v1"
 const busca = (cidade) => { 
       axios.get(`${url1}/search?name=${cidade}&count=1&language=pt&format=json`).then(res => {
       json=res.data
-      console.log(json)
-      latitude = json.results[0].latitude
-      longitude = json.results[0].longitude
+      let latitude = json.results[0].latitude
+      let longitude = json.results[0].longitude
       let name= json.results[0].name
-      console.log(`A cidade é ${name} e sua longitude é ${longitude} e sua latitude é ${latitude}`)
+      console.log(`\n\n-----Informações do local-----\n\nCidade: ${name}\nLongitude: ${longitude}\nLatitude: ${latitude}`)
+      condicoes(latitude, longitude)
     }).catch(err => {
       console.log(`Aconteceu um erro.\n ${err}`)
     })
 }
 
-busca("são paulo")
+const condicoes = async (latitude, longitude) => {
+  try {
+    let res = await axios.get(`${url2}/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,apparent_temperature,wind_speed_10m&forecast_days=1`)
+    let json=res.data
+    let temperatura= json.current.temperature_2m
+    let sensacao= json.current.apparent_temperature
+    let vento= json.current.wind_speed_10m
+    console.log(`\n\n-----Informações do clima-----\n\nTemperatura: ${temperatura} graus\nSensação térmica: ${sensacao} graus\nVelocidade do vento a 10 metros: ${vento}km/h `)
+  } catch (err){
+    console.log(`Aconteceu um erro.\n ${err}`)
+  }
+}
+busca("paris")
